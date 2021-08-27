b29dee0603f0cc087c05779b8a66f8fa0f30086e
Назаров Максим Олегович (000086982) <maksim.nazarov@bia-tech.ru>
2021-08-27 13:20:05 +0300

    tut6

--- src/main/java/com/mrHyde30/rabbitSpring/tut6Config.java
+++ src/main/java/com/mrHyde30/rabbitSpring/tut6Config.java
@@ -1,4 +1,29 @@
 package com.mrHyde30.rabbitSpring;
 
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Profile("tut6")
+@Configuration
 public class tut6Config {
+
+    @Profile("server")
+    public static class ClientConfig{
+
+        @Bean
+        public DirectExchange direct() {
+            return new DirectExchange("tut.rpc");
+        }
+
+
+
+    }
+
+    @Profile("client")
+    public static class ServerConfig{
+
+    }
+
 }
